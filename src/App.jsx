cd4d8db87d522beab99b9139c4d1b404471e3d9d
Tamilmozhi <Tamilmozhi2003@gmail.com>
2026-06-11 15:00:36 +0530

    Updated project with latest changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+
+
 import Auth from "./components/Auth";
+import React,{useEffect} from 'react';
+import './App.css';
+import { HashRouter,Routes,Route } from 'react-router-dom';
+
 
 function App() {
-  return <Auth />;
+
+  return (
+    <>
+    
+
+       <HashRouter><Routes>
+        <Route path="/" element={<Auth/>}/>
+        
+      </Routes></HashRouter>
+
+    </>
+  );
 }
 
 export default App;
